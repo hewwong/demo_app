a34228b8923dccb62107b0c3c8f83f11dc9e05a6
hew <olaio@qq.com>
2014-02-18 14:36:06 +0800

    Initial commit

--- rails_projects/first_app/config/initializers/secret_token.rb
+++ rails_projects/first_app/config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-FirstApp::Application.config.secret_key_base = 'dcfafe384496f7f5cf51b1948932ce5191a31dc5aec1139c918221133facf520fd4cea3e93619613a5591ab2c337edc2a8fd394cad3c289da2ac99d33017eb5d'
+FirstApp::Application.config.secret_key_base = '48a7bc329eca16054c9920de2201c4c07b2d52e21ddc52059154c99a96113d5738391a9be8ea50a54d2727ab1bb6e9af013a4c4fe791238f3ddb6d3650bb5859'
